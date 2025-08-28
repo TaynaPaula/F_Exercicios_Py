b366642dcdcd9063e7e86c957bd6ebbf00664bf9
TaynaPaula <taynapaulacruz@gmail.com>
2025-08-28 17:00:31 -0300

    Update teste14082025.py

--- teste14082025.py
+++ teste14082025.py
@@ -98,4 +98,31 @@ while n < 100 or n > 500:
             print("o valor lido {} esta ok" .format(n));
     finally:
         print("\n\n");#repetição do código
-        
+
+
+#*************
+x = "abcd";
+print( x[-1]);
+print( x[-2]);
+print( x[-3]);
+
+print( x[0]);
+print( x[1]);
+print( x[2]);
+
+print( x[1]);
+print( x[2]);
+print( x[3]);
+
+s ='cadeia de texto definido com apostofro';
+d = "cadeia de texto definido com aspas";
+print (s);
+print(d);
+print(type(d));
+print(type(s));
+print(len(s));#quantidade total de string, fila
+print(s[0]);
+print(s[1]);
+print(s[37]);
+print(s[40]); #deu erro pq o numero de letras é menor
+
