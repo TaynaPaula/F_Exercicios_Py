baa182fff8f688640b08e5017c7c009ba1f631d3
TaynaPaula <taynapaulacruz@gmail.com>
2025-08-28 17:41:17 -0300

    Update teste14082025.py

--- teste14082025.py
+++ teste14082025.py
@@ -149,19 +149,20 @@ s= 'abcdefghijklmno'
 print(s)
 len(s)
 print(s[3:10])
-print(s[0:5])
+print(s[0:5]) #o mesmo valor abcde pq tanto faz colocar o zero
 p = s[3:10]
 print (p)
 print(len(p))
 i = 0
 f=0
 print(s[i:f])
-print(s[:5])
+print(s[:5])#o mesmo valor abcde pq tanto faz colocar o zero
 print(s[5:])
 
 t='9pula8pula7pula6pula5'
 print(len(t))
-print(t[0:21:5])
-print(t[::5])
+print(t[0:21:5])# de cinco em cinco
+print(t[::5])# de cinco em cinco
+
 
 
